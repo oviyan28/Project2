fb3c9ee108814924a704a2a7aac21b70deab6392
Dell <Dell@192.168.1.19>
2019-01-29 12:22:08 +0530

    29/02/19

--- Project2Backend/src/main/java/com/niit/Configuration/WebAppConfig.java
+++ Project2Backend/src/main/java/com/niit/Configuration/WebAppConfig.java
@@ -1,7 +1,17 @@
 package com.niit.Configuration;
 
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+@Configuration
+@EnableWebMvc //similar to <mvc:annotation-driven> in dispatcher-servlet.xml
+@ComponentScan(basePackages="com.niit")//similar to <context:component-scan> in dispatcher-servlet.xml 
 
 public class WebAppConfig extends WebMvcConfigurerAdapter {
+	
+		public WebAppConfig(){
+			System.out.println("WebAppConfig class is loaded and instantiated");
+		}
+	}
 
-}
