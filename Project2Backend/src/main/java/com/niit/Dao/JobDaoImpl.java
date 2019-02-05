cef40d4b93a730ce962338ee1ac6ff9dbe146523
Dell <Dell@192.168.1.13>
2019-02-05 11:44:15 +0530

    5/02/2019

--- Project2Backend/src/main/java/com/niit/Dao/JobDaoImpl.java
+++ Project2Backend/src/main/java/com/niit/Dao/JobDaoImpl.java
@@ -4,12 +4,14 @@ package com.niit.Dao;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.niit.models.Job;
-@Repository
 @Transactional
+@Repository
+
 public class JobDaoImpl implements JobDao {
 @Autowired
 private SessionFactory sessionFactory;
--- project2client/WebContent/services/jobservice.js
+++ project2client/WebContent/services/jobservice.js
@@ -2,16 +2,11 @@
  * 
  */
 app.factory('JobService',function($http){
-  //create an object, add functions, return object
-	
+  
 	var jobService={}
-	//addJob in service   <- addJob in controller   <-  jobform.html
-	jobService.addJob=function(job){//get job object from JobCtrl ,JObCtrl will get it from view
-		//$http.post(url,data)
-		var url="http://localhost:8080/project2middleware/addjob"
-		//call restful service
-		return $http.post(url,job)//job in JSON format, will get added in the body of Http request
-	}
+	jobService.addJob=function(job){	
+		var url="http://localhost:8080/Project2middleware/addjob"
+		return $http.post(url,job)}
 	
 	
 	return jobService;
